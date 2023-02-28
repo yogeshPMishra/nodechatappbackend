92ad5912c37a06519b08f4ec61964eac605e1693
yogeshPMishra <mail.yogeshpmishra@gmail.com>
2023-02-28 17:36:06 +0530

    master one

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
-const io = require('socket.io')(8000,{cors: {origin: "*"}})
+const io = require('socket.io')(8000,{cors: {origin: "*"}},()=>{
+    console.log('server run on port no 8000');
+})
 const  users = {};
 io.on('connection',socket =>{
     socket.on('new-user-joined', name =>{
@@ -12,9 +14,11 @@ io.on('connection',socket =>{
         users[socket.id] = name;
         socket.broadcast.emit('user-joined',name)
     })
+    
     socket.on('send', message=>{
         socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
     })
+
     socket.on('disconnect',message =>{
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id]
